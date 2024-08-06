79736fadea64d5e988d9bee05a792c6a90cc7e03
Nduka Ofoeyeno <nofoeyeno@gmail.com>
2024-08-06 02:55:50 +0100

    setting for deployment

--- CapitalCompass/settings.py
+++ CapitalCompass/settings.py
@@ -173,7 +173,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default='')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = 'Capital.Compass12@gmail.com'
 CONTACT_EMAIL = 'Capital.Compass12@gmail.com'
