b1fa675a03c38e38f3035133786e53ee08f47162
Nduka Ofoeyeno <nofoeyeno@gmail.com>
2024-08-06 14:07:03 +0100

    setting for production

--- CapitalCompass/settings_local.py
+++ CapitalCompass/settings_local.py
@@ -6,11 +6,5 @@ SECRET_KEY = env('DJANGO_SECRET_KEY', default='your-local-secret-key')
 DEBUG = True
 ALLOWED_HOSTS = []
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
-SITE_ID = 2
+SITE_ID = 1
