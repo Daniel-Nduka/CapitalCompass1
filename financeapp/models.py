82cbcccefbe43e7e097ff66fff9274abb76d482f
Nduka Ofoeyeno <nofoeyeno@gmail.com>
2024-08-12 15:28:46 +0000

    production fixes

--- financeapp/models.py
+++ financeapp/models.py
@@ -14,29 +14,29 @@ class UserProfile(models.Model):
     last_accessed_budget = models.ForeignKey('Budget', on_delete=models.SET_NULL, null=True, blank=True)
     def __str__(self):
         return self.user.username
-    
+
 #Budget Model
 class Budget(models.Model):
     BUDGET_TYPES = [
         ('zero_based', 'Zero-Based'),
         ('fifty_thirty_twenty', '50/30/20'),
     ]
-   
+
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='budgets')
     budget_name = models.CharField(max_length=100)
     budget_type = models.CharField(max_length=20, choices=BUDGET_TYPES)
     month = models.DateField(auto_now_add=True)  # Add month field
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-   # balance_initialized = models.BooleanField(default=False)  
-    
+   # balance_initialized = models.BooleanField(default=False)
+
     # Meta class to ensure that the budget name is unique for each user
     class Meta:
         unique_together = ('user', 'budget_name')
-        
+
     def __str__(self):
         return f"{self.budget_name} ({self.get_budget_type_display()}) - {self.user.username}"
-    
+
     # Update the assigned amount for each category in the 50/30/20 budget
     def update_fifty_thirty_twenty_categories(self):
         if self.budget_type == 'fifty_thirty_twenty':
@@ -50,14 +50,14 @@ class Budget(models.Model):
                 else:
                     category.assigned_amount = Decimal(0.2 * total_balance_float)
                 category.save()
-            
+
 
 
 class Account(models.Model):
     ACCOUNT_TYPES = [
        ('CHECKING', 'checking'),
        ('SAVINGS', 'savings'),
-      # ('CREDIT', 'credit'), 
+      # ('CREDIT', 'credit'),
        ('CASH', 'cash'),
     ]
     budget = models.ForeignKey(Budget, on_delete=models.CASCADE, related_name='accounts')
@@ -67,15 +67,15 @@ class Account(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    
-    
+
+
     def __str__(self):
        return f"{self.account_name} ({self.get_account_type_display()}) - {self.budget.budget_name}"
-   
+
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         self.budget.update_fifty_thirty_twenty_categories()
-        
+
 @receiver(post_save, sender=Account)
 def create_initial_transaction(sender, instance, created, **kwargs):
     if created:
@@ -86,14 +86,14 @@ def create_initial_transaction(sender, instance, created, **kwargs):
             inflow=instance.balance,
             outflow=0,
             created_by_account=True,
-        ) 
+        )
 
 #Base Category
 class BaseCategory(models.Model):
     assigned_amount = models.DecimalField(max_digits=10, decimal_places=2)
     month = models.DateField(default=datetime.date.today().replace(day=1))
-    
-    
+
+
     class Meta:
         abstract = True
     def __str__(self):
@@ -106,7 +106,7 @@ class BaseCategory(models.Model):
     @property
     def available(self):
         return self.assigned_amount - self.activity
-    
+
     @property
     def expenses_assigned_amount_total(self):
         return sum(expense.assigned_amount for expense in self.expenses.all())
@@ -128,18 +128,18 @@ class FiftyThirtyTwentyCategory(BaseCategory):
 
     budget = models.ForeignKey(Budget, on_delete=models.CASCADE, related_name='fifty_thirty_twenty_categories')
     name = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
-   # month_only = models.DateField(default=datetime.date.today().replace(day=1))
     is_recurring = models.BooleanField(default=True)  # New field to indicate if category is occuring
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     month_only = models.DateField(default=datetime.date.today().replace(day=1))
 
+
     def __str__(self):
         return f"{self.name} - {self.budget.budget_name} ({self.month.strftime('%B %Y')})"
 
     class Meta:
-        unique_together = ('budget', 'name', 'month')   
-  
+        unique_together = ('budget', 'name', 'month')
+
 @receiver(post_save, sender=Budget)
 def create_fifty_thirty_twenty_categories(sender, instance, created, **kwargs):
     if created and instance.budget_type == 'fifty_thirty_twenty':
@@ -150,19 +150,19 @@ def create_fifty_thirty_twenty_categories(sender, instance, created, **kwargs):
                 name=name,
                 assigned_amount=0.0,
                 month=instance.month
-                
+
             )
- 
- 
-#Zero Based Models implementation      
+
+
+#Zero Based Models implementation
 class ZeroBasedCategory(BaseCategory):
     budget = models.ForeignKey(Budget, on_delete=models.CASCADE, related_name='zero_based_categories')
-    name = models.CharField(max_length=100) 
+    name = models.CharField(max_length=100)
     month_only = models.DateField(default=datetime.date.today().replace(day=1))
     is_recurring = models.BooleanField(default=False)
     class Meta:
         unique_together = ('budget', 'name', 'month')
-        
+
     def __str__(self):
         return self.name
 
@@ -173,20 +173,20 @@ class Expense(models.Model):
     date = models.DateField(auto_now_add=True)
     is_recurring = models.BooleanField(default=False)  # New field to indicate if the expense is recurring
     month_only = models.DateField(default=datetime.date.today().replace(day=1))
-    
+
     assigned_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     spent = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    
+
     class Meta:
         unique_together = ('fifty_30_twenty_category', 'description', 'month_only'), ('category', 'description', 'month_only')
-        
+
 
     def __str__(self):
         return f"{self.assigned_amount} - {self.description}"
-    @property   
+    @property
     def available(self):
         return self.assigned_amount - self.spent
-  
+
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         # Update the category's assigned amount if necessary
@@ -195,8 +195,8 @@ class Expense(models.Model):
         if self.fifty_30_twenty_category:
             self.fifty_30_twenty_category.save()
 
-   
-           
+
+
 class Transaction(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='transactions')
     expense = models.ForeignKey(Expense, on_delete=models.CASCADE, related_name='transactions', null=True, blank=True)
@@ -205,8 +205,8 @@ class Transaction(models.Model):
     payee = models.CharField(max_length=255, blank=True, null=True)
     inflow = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     outflow = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    created_by_account = models.BooleanField(default=False) 
-    
+    created_by_account = models.BooleanField(default=False)
+
     def __str__(self):
         return f"{self.date} - {self.description} ({self.inflow} / {self.outflow})"
 
@@ -255,7 +255,7 @@ class ContactMessage(models.Model):
 
     def __str__(self):
         return f"Message from {self.name} - {self.email}"
-    
+
 class AccountSupport(models.Model):
     PROBLEM_CHOICES = [
         ('General', 'General'),
@@ -272,5 +272,4 @@ class AccountSupport(models.Model):
 
     def __str__(self):
         return f"Message from {self.user.username}"
-    
-        
+
